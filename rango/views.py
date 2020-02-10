67e5c1bbb2b6fce3fd808f6ec4afa21bc22712b9
2320395g <2320395g@student.gla.ac.uk>
2020-02-10 01:55:33 +0000

    Fixed a few things causing test 4 failures + some changes attempting to fix task 6 failure.

--- rango/views.py
+++ rango/views.py
@@ -9,13 +9,12 @@ from rango.forms import CategoryForm, PageForm
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
-            'categories' : category_list, 'pages':page_list}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    
     return render(request,'rango/index.html', context = context_dict)
 
 
